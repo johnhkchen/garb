cb2f0a9555e16cddd3506c3fd7b055fcf4748e20
John Chen <john.hk.chen@gmail.com>
2022-11-30 19:27:25 +0000

    Add our first website!

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,19 +1,37 @@
 use yew::prelude::*;
+use yew::{function_component, html, Html};
 
 #[function_component]
 fn App() -> Html {
     let counter = use_state(|| 0);
-    let onclick = {
+    
+    let add_1= {
         let counter = counter.clone();
         move |_| {
             let value = *counter + 1;
             counter.set(value);
         }
     };
+    let add_10= {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 10;
+            counter.set(value);
+        }
+    };
+    let add_25 = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 25;
+            counter.set(value);
+        }
+    };
 
     html! {
         <div>
-            <button {onclick}>{ "+1" }</button>
+            <button onclick={add_1}>{ "+1" }</button>
+            <button onclick={add_10}>{ "+10" }</button>
+            <button onclick={add_25}>{ "+25" }</button>
             <p>{ *counter }</p>
         </div>
     }
