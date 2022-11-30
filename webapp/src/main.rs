df8255b5fa279277be1a95c4959524833ad698af
John Chen <john.hk.chen@gmail.com>
2022-11-30 17:41:30 +0000

    Add yew web app to project

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,3 +1,24 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello! I am Garb, and I will soon be a web app!");
+    yew::Renderer::<App>::new().render();
 }
